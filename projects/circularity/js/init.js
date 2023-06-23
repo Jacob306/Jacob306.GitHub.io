5fd907995a26a173900fcc5d78801093630b3326
Jacob Johnson <jjper7898@gmail.com>
2023-06-23 17:01:14 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,18 +26,15 @@ var init = function (window) {
        
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 10 ,10);
             view.addChild(circle);
             circles.push(circle);
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-
+        for (var i = 0; i < 100; i++) {
+            drawCircle()
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -50,17 +47,15 @@ var init = function (window) {
         function update() {
          
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition( circles[0] ) 
-            physikz.updatePosition( circles[1] ) 
-            physikz.updatePosition( circles[2] ) 
-            physikz.updatePosition( circles[3] ) 
-            physikz.updatePosition( circles[4] ) 
+        for (var i = 0; i < circles.length; i++) {
+            physikz.updatePosition( circles[i] ) 
+}
+
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           game.checkCirclePosition (circles [0])
-           game.checkCirclePosition (circles [1])
-           game.checkCirclePosition (circles [2])
-           game.checkCirclePosition (circles [3])
-           game.checkCirclePosition (circles [4])
+            for (var i = 0; i < circles.length; i++) {
+                game.checkCirclePosition (circles [i])
+            }
+        
             // TODO 9 : Iterate over the array
            
             
